ad3b9d1400063da3355eb75c2d866d6044d2c2c5
ferytiggy <106712354+ferytiggy@users.noreply.github.com>
2022-12-12 18:30:09 -0600

    Add files via upload

---
+++ salas.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAR2Rm2T7l3cI1pQ495H1z68xGQx-RrEK4",
+    authDomain: "tiggwitter.firebaseapp.com",
+    databaseURL: "https://tiggwitter-default-rtdb.firebaseio.com",
+    projectId: "tiggwitter",
+    storageBucket: "tiggwitter.appspot.com",
+    messagingSenderId: "814916041516",
+    appId: "1:814916041516:web:d294fa2a88daab12e48a89"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
